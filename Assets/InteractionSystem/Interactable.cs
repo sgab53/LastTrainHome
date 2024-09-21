b2904f065891f1b9d569af3fce44686189295acd
Simone <simoneippoliti53@gmail.com>
2024-09-21 10:44:50 +0200

    Implement interaction system

--- Assets/InteractionSystem/Interactable.cs
+++ Assets/InteractionSystem/Interactable.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Collider2D))]
 public class Interactable : MonoBehaviour
 {
     [SerializeField] private UnityEvent _onActivate;
+
+    private Collider2D _trigger;
+
+    private void OnValidate() {
+        if (!_trigger)
+            _trigger = (Collider2D)GetComponent(typeof(Collider2D));
+
+        _trigger.isTrigger = true;
+    }
 
     public void Activate()
     {
--- Assets/InteractionSystem/Interactor.cs
+++ Assets/InteractionSystem/Interactor.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CircleCollider2D))]
 public class Interactor : MonoBehaviour
 {
     [SerializeField] private float _activationRange = 2f;
+    [SerializeField] private CircleCollider2D _sensor;
 
     private Interactable _cachedInteractable;
 
+    private void OnValidate()
+    {
+        if (!_sensor)
+            _sensor = (CircleCollider2D)GetComponent(typeof(CircleCollider2D));
+
+        _sensor.radius = _activationRange;
+        _sensor.isTrigger = true;
+    }
+
     private void Update()
     {
-        if (Input.GetButton("Action"))
+        if (Input.GetButtonDown("Action"))
         {
             _cachedInteractable.Activate();
             _cachedInteractable = null;
@@ -35,7 +46,10 @@ public class Interactor : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
+        if (!_sensor)
+            return;
+
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, _activationRange);
+        Gizmos.DrawWireSphere(_sensor.transform.position, _activationRange);
     }
 }

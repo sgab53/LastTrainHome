cbd480c9ae46e062f9cf0cd501223e42d8c01b04
Jaeger <rosati.1595834@gmail.com>
2024-09-21 15:36:04 +0200

    New text importer

--- tools/notion_text_importer.py
+++ tools/notion_text_importer.py
@@ -1,15 +1,30 @@
 from notion2pandas import Notion2PandasClient
 import json
-
-
+import ast
 
 with open('secrets.json', 'r') as notionFile:
     notion_data = json.load(notionFile)
     token = notion_data.get('notion_api_token')
-    database_id = notion_data.get(
+    database_texts_id = notion_data.get(
         'gioco_jam').get('id_database_text')
+    database_dialogs_id = notion_data.get(
+        'gioco_jam').get('id_database_dialog')
 
 n2p = Notion2PandasClient(auth=token)
-df = n2p.from_notion_DB_to_dataframe(database_id)
+dfText = n2p.from_notion_DB_to_dataframe(database_texts_id)
 
-df.to_csv('texts.csv',columns=['key', 'it', 'en'], index=False)
+transformed_data = {row['key']: {"it": row['it'], "en": row['en']} for _, row in dfText.iterrows()}
+json_result = json.dumps(transformed_data, indent=4)
+
+
+with open('texts.json', 'w') as f:
+    f.write(json_result)
+
+dfdialog = n2p.from_notion_DB_to_dataframe(database_dialogs_id)
+
+dfdialog['testi'] = dfdialog['testi'].apply(ast.literal_eval)
+transformed_data = {row['key']: {"testi": row['testi']} for _, row in dfdialog.iterrows()}
+json_result = json.dumps(transformed_data, indent=4)
+
+with open('outputdialogo.json', 'w') as f:
+    f.write(json_result)

7a88c9b26da58236bf1e8d7907d3ec10aa77fb89
Jaeger <rosati.1595834@gmail.com>
2024-09-22 10:38:15 +0200

    Fix notion importer

--- tools/notion_text_importer.py
+++ tools/notion_text_importer.py
@@ -14,7 +14,7 @@ n2p = Notion2PandasClient(auth=token)
 dfText = n2p.from_notion_DB_to_dataframe(database_texts_id)
 
 transformed_data = {row['key']: {"it": row['it'], "en": row['en']} for _, row in dfText.iterrows()}
-json_result = json.dumps(transformed_data, indent=4)
+json_result = json.dumps(transformed_data, indent=4, ensure_ascii=False)
 
 
 with open('Assets/DialogueSystem/Resources/texts.json', 'w') as f:

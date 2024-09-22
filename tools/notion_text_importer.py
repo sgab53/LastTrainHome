59cf026810be2095c33e7578f5d617284af667f0
Jaeger <rosati.1595834@gmail.com>
2024-09-22 16:26:46 +0200

    Fix text importer

--- tools/notion_text_importer.py
+++ tools/notion_text_importer.py
@@ -13,18 +13,18 @@ with open('tools/secrets.json', 'r') as notionFile:
 n2p = Notion2PandasClient(auth=token)
 dfText = n2p.from_notion_DB_to_dataframe(database_texts_id)
 
-transformed_data = {row['key']: {"it": row['it'], "en": row['en']} for _, row in dfText.iterrows()}
+transformed_data = {'_data':{row['key']: {"it": row['it'], "en": row['en']} for _, row in dfText.iterrows()}}
 json_result = json.dumps(transformed_data, indent=4, ensure_ascii=False)
 
 
-with open('Assets/DialogueSystem/Resources/texts.json', 'w') as f:
+with open('Assets/DialogueSystem/Resources/texts.json', 'w', encoding='utf8') as f:
     f.write(json_result)
 
 dfdialog = n2p.from_notion_DB_to_dataframe(database_dialogs_id)
 
 dfdialog['testi'] = dfdialog['testi'].apply(ast.literal_eval)
-transformed_data = {row['key']: {"testi": row['testi']} for _, row in dfdialog.iterrows()}
-json_result = json.dumps(transformed_data, indent=4)
+transformed_data = {'_data':{row['key']: row['testi'] for _, row in dfdialog.iterrows()}}
+json_result = json.dumps(transformed_data, indent=4, ensure_ascii=False)
 
-with open('Assets/DialogueSystem/Resources/dialogues.json', 'w') as f:
+with open('Assets/DialogueSystem/Resources/dialogues.json', 'w', encoding='utf8') as f:
     f.write(json_result)

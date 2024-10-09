88a0a85b2b04421aa2f123e60d018a883282df59
Simone <simoneippoliti53@gmail.com>
2024-10-09 22:17:14 +0200

    Clean up the Assets directory

--- Assets/Enemy/Scripts/EnviromentGraph.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnviromentGraph : MonoBehaviour
-{
-    [SerializeField] public List<EnviromentPoint> points = new List<EnviromentPoint>();
-}

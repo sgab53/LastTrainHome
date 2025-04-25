5d209fa0221f0b943a37795e54bc31f950a9535b
Simone <simoneippoliti53@gmail.com>
2025-04-25 22:58:11 +0200

    Rename DialogueTrigger to DialogueInteraction

---
+++ Assets/DialogueSystem/Scripts/DialogueInteraction.cs
@@ -0,0 +1,70 @@
+using LTH.Core.Services;
+using LTH.InteractionSystem;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace LTH.DialogueSystem
+{
+    public sealed class DialogueInteraction : Interactable
+    {
+        [SerializeField] private AssetReferenceT<DialogueEntry> _dialogueReference;
+        [SerializeField] private DialogueTriggerActivation _activation;
+
+        [Header("Optional")]
+        [SerializeField] private DialogueService _dialogueService;
+
+        private DialogueEntry _entry;
+
+        public override void Interact()
+        {
+            if (!enabled)
+                return;
+
+            base.Interact();
+            _dialogueService.StartDialogue(_entry);
+            this.enabled = false;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _entry =
+                Addressables.LoadAssetAsync<DialogueEntry>(_dialogueReference.AssetGUID).WaitForCompletion();
+
+            if (!_dialogueService)
+                _dialogueService = Service.Load<DialogueService>();
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            _dialogueService.DialogueEnded += OnDialogueEnded;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            _dialogueService.DialogueEnded -= OnDialogueEnded;
+        }
+
+        private void OnDialogueEnded()
+        {
+            switch (_activation)
+            {
+                case DialogueTriggerActivation.Once:
+                    break;
+                case DialogueTriggerActivation.Repeat:
+                    this.enabled = true;
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        private enum DialogueTriggerActivation : byte
+        {
+            Once, Repeat
+        }
+    }
+}

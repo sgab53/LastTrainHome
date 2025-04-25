c14151ea054e99a79658e41dbd77be752e766d49
Simone <simoneippoliti53@gmail.com>
2025-04-25 22:57:27 +0200

    Improve interaction system by using service assets

--- Assets/InteractionSystem/Scripts/Interactor.cs
+++ Assets/InteractionSystem/Scripts/Interactor.cs
@@ -1,4 +1,3 @@
-using Cysharp.Threading.Tasks;
 using LTH.Core.Services;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,13 +7,21 @@ namespace LTH.InteractionSystem
     public sealed class Interactor : MonoBehaviour
     {
         [SerializeField] private InputActionReference _interact;
-        private InteractionService _interactionService;
 
         [Header("Parameters")]
         [SerializeField] private float _interactionRadius = 3f;
         [SerializeField] private LayerMask interactableLayers;
 
+        [Header("Optional")]
+        [SerializeField] private InteractionService _interactionService;
+
         private readonly Collider[] _hits = new Collider[8];
+
+        private void Awake()
+        {
+            if (!_interactionService)
+                _interactionService = Service.Load<InteractionService>();
+        }
 
         private void OnEnable()
         {
@@ -24,16 +31,6 @@ namespace LTH.InteractionSystem
         private void OnDisable()
         {
             _interact.action.performed -= OnInteractActionPerformed;
-        }
-
-        private void Awake()
-        {
-            InitInteractor().Forget();
-        }
-
-        private async UniTaskVoid InitInteractor()
-        {
-            _interactionService = await Service.Get<InteractionService>();
         }
 
         private void OnInteractActionPerformed(InputAction.CallbackContext _)
